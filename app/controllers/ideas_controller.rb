1bf35fac7e01d0f708357442dc6f4ed000e903b2
Dibyan Maharjan <dibyan@acd.edu.np>
2017-02-21 17:52:51 +0000

    Moving the form into a Modal

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,5 +2,14 @@ class IdeasController < ApplicationController
     def index
         @ideas = Idea.all
     end
-
+    def create
+        @idea = Idea.create(idea_params) 
+        #(description: params[:idea][:description], author: params[:idea][:author])
+        redirect_to root_path
+    end
+    private 
+    def idea_params 
+        params.require(:idea).permit(:description, :author)
+    end
+    
 end
