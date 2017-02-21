28ce91cc17b94f6679fb6ec5b8f4a046dd3115fb
Dibyan Maharjan <dibyan@acd.edu.np>
2017-02-21 18:13:30 +0000

    Editing and Updating Ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -7,6 +7,18 @@ class IdeasController < ApplicationController
         #(description: params[:idea][:description], author: params[:idea][:author])
         redirect_to root_path
     end
+    def edit
+        @idea = Idea.find(params[:id])
+    end
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to root_path
+        else
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
+    
     private 
     def idea_params 
         params.require(:idea).permit(:description, :author)
