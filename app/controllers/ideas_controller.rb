e35e5b882fdd49b2988f4712c36562414c83b1dc
Dibyan Maharjan <dibyan@acd.edu.np>
2017-02-27 10:03:49 +0000

    Some Updates

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
     def index
-        @ideas = Idea.all.paginate(:page => params[:page], :per_page => 5)
+        @ideas = Idea.order("created_at DESC").paginate(:page => params[:page], :per_page => 4)
     end
     
     def create
